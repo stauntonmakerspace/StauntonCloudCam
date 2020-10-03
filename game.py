5d0789b42cb820d6408d412a903e06fe0c7dda41
Nile Walker <niwal7@morgan.edu>
2020-10-02 20:56:17 -0400

    Added Drag Drop Control

---
+++ game.py
@@ -0,0 +1,111 @@
+import paho.mqtt.client as mqtt  # import the client
+import struct
+import pygame
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED = (255,   0,   0)
+
+FPS = 30
+
+pygame.init()
+
+pygame.display.set_caption("Tracking System")
+
+# - objects -
+
+rectangle = pygame.rect.Rect(176, 134, 17, 17)
+slider = pygame.rect.Rect(176, 134, 17, 17)
+slider_draging = False
+rectangle_draging = False
+
+# - mainloop -y
+
+
+broker_address = "127.0.0.1"
+client = mqtt.Client("Master")  # create new instance
+client.connect(broker_address)  # connect to broker
+client.loop_start()
+#    Width
+# A ------- B
+# |         |
+# |         | Length
+# |         |
+# D ------- C   # Height = Distance from the floor
+Length, Width, Height = 41.91, 55.88, 30  # cm
+# --- constants --- (UPPER_CASE names)
+
+SCREEN_WIDTH = int(Width) * 10
+W_R = Width / SCREEN_WIDTH 
+SCREEN_HEIGHT = int(Height) * 10
+H_R = Height / SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def distance(x1, y1, z1, x2, y2, z2):
+    return round(((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)**.5,2)
+
+def drawGrid():
+    blockSize = 60 #Set the size of the grid block
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
+            rect = pygame.Rect(x*blockSize, y*blockSize,
+                               blockSize, blockSize)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+
+pygame.font.init() # you have to call this at the start, 
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render('A', False, RED)
+# TODO: Add Calibration Phase 
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    # - events -
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if rectangle.collidepoint(event.pos):
+                    rectangle_draging = True
+                    mouse_x, mouse_y = event.pos
+                    offset_x = rectangle.x - mouse_x
+                    offset_y = rectangle.y - mouse_y
+                #elif slider: pass
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                rectangle_draging = False
+
+        elif event.type == pygame.MOUSEMOTION:
+            if rectangle_draging:
+                mouse_x, mouse_y = event.pos
+                rectangle.x = mouse_x + offset_x
+                rectangle.y = mouse_y + offset_y
+                z = 0 
+                new_lengths = [distance(rectangle.x * W_R, rectangle.y * H_R, z, x2, y2, z2) for x2, y2, z2 in [
+                    (0, 0, 0), (Width, 0, 0), (Width, Length, 0), (0, Length, 0)]]
+                for name, length in zip(["A", "B", "C", "D"], new_lengths):
+                    client.publish(name, struct.pack('f', length))  # Publish
+                    print(f"{name} Target: {length} cm")
+
+    # - draws (without updates) -
+
+    screen.fill(WHITE)
+    # drawGrid()
+
+    pygame.draw.rect(screen, RED, rectangle)
+
+    pygame.display.flip()
+
+    # - constant game speed / FPS -
+
+    clock.tick(FPS)
+    
+
+# - end -
+client.loop_stop()
+pygame.quit()
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import struct
 import paho.mqtt.client as mqtt  # import the client
 
-def distance(x1, y1, x2, y2):
-    return ((x1 - x2)**2 + (y1 - y2)**2)**.5
+
+def distance(x1, y1, z1, x2, y2, z2):
+    return ((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)**.5
+
 
 broker_address = "127.0.0.1"
 client = mqtt.Client("Master")  # create new instance
@@ -10,17 +12,20 @@ client.connect(broker_address)  # connect to broker
 #    Width
 # A ------- B
 # |         |
-# |         | Height
+# |         | Length
 # |         |
-# D ------- C
-Height, Width = 16.5, 22  # Inches
+# D ------- C   # Height = Distance from the floor
+Length, Width, Height = 41.91, 55.88, 30  # cm
 # TODO: Add Calibration Phase
 while True:
-    x, y = input("Enter X Y\n")
-    if x > Width and y > Height:
+    client.loop_start()  # Start the Loop
+    x, y, z = [int(i) for i in input("Enter X Y Z within bounds: \n").split()]
+    if 0 > x > Width and 0 > y > Length and 0 > z > Height:
+        print("Out of Bounds \n")
         continue
-    new_lengths = [distance(x, y, x2, y2) for x2, y2 in [
-        (0, 0), (Width, 0), (Width, Height), (0, Height)]]
+    new_lengths = [distance(x, y, z, x2, y2, z2) for x2, y2, z2 in [
+        (0, 0, 0), (Width, 0, 0), (Width, Length, 0), (0, Length, 0)]]
     for name, length in zip(["A", "B", "C", "D"], new_lengths):
-        client.publish(name, struct.pack('f', length))  # publish
-        # float x = *(float *)&float_temp;
+        client.publish(name, struct.pack('f', length))  # Publish
+        print(f"{name} Target: {length} cm")
+    client.loop_stop()  # Stop the Loop
